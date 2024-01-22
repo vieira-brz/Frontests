b440c47a4b6a4a6ad1bd83f7424df2747dfccbdc
vieira-brz <vieirabraz2003@gmail.com>
2024-01-22 11:12:42 -0300

    Navegação React

---
+++ rotas/src/components/layout/Menu.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import './Menu.css'
+
+import { Link } from 'react-router-dom'
+
+const Menu = props => (
+    <aside className="Menu">
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Início</Link>
+                </li>
+                <li>
+                    <Link to="/about">Sobre</Link>
+                </li>
+                <li>
+                    <Link to="/param/123">Param 01</Link>
+                </li>
+                <li>
+                    <Link to="/param">Param 02</Link>
+                </li>
+                <li>
+                    <Link to="/forget">404</Link>
+                </li>
+            </ul>
+        </nav>
+    </aside>
+)
+
+export default Menu
