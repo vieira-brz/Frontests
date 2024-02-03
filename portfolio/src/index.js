9aebb0dcbf717f5f222fd37e9123b3d0d801f950
vieira-brz <vieirabraz2003@gmail.com>
2024-02-03 10:38:28 -0300

    portfolio

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.scss'
+import ReactDOM from 'react-dom'
+import React from 'react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Css
+import 'normalize.css'
+import '@fontsource/poppins'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Import App
+import App from './views/App'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
